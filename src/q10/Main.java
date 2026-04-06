2bbca33faf7535507bc92adc49664b3ecf3ca465
Sunny-lab514 <vermasunny502@gmail.com>
2026-04-07 00:01:16 +0530

    Q10 _ Solve_By_ Sunny

--- src/q10/Main.java
+++ src/q10/Main.java
@@ -3,7 +3,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] tokens = line.trim().split(" ");
+int[] stack = new int[tokens.length];
+int top = -1;
 
+for (String token : tokens) {
+    // Check if operator
+    if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+        
+        int b = stack[top--];
+        int a = stack[top--];
+        int result = 0;
+
+        if (token.equals("+")) result = a + b;
+        else if (token.equals("-")) result = a - b;
+        else if (token.equals("*")) result = a * b;
+        else if (token.equals("/")) result = a / b;
+
+        stack[++top] = result;
+
+    } else {
+        // Operand
+        int num = Integer.parseInt(token);
+        stack[++top] = num;
+    }
+}
+
+// Final result
+System.out.println(stack[top]);
         // TODO: Evaluate a postfix (Reverse Polish Notation) expression
         //       Operands are non-negative integers, operators are: + - * /
         //       Use a stack to evaluate
