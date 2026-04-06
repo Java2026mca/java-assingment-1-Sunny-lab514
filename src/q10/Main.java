0aa66f8b63a9060f8ee8481175f5ed09db47d5ac
Sunny-lab514 <vermasunny502@gmail.com>
2026-04-07 00:12:59 +0530

    Q10 _ Solve_By_ Sunny

--- src/q10/Main.java
+++ src/q10/Main.java
@@ -3,34 +3,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+
         String[] tokens = line.trim().split(" ");
-int[] stack = new int[tokens.length];
-int top = -1;
+        int[] stack = new int[tokens.length];
+        int top = -1;
 
-for (String token : tokens) {
-    // Check if operator
-    if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
-        
-        int b = stack[top--];
-        int a = stack[top--];
-        int result = 0;
+        for (String token : tokens) {
+            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
 
-        if (token.equals("+")) result = a + b;
-        else if (token.equals("-")) result = a - b;
-        else if (token.equals("*")) result = a * b;
-        else if (token.equals("/")) result = a / b;
+                int b = stack[top--];
+                int a = stack[top--];
+                int result = 0;
 
-        stack[++top] = result;
+                if (token.equals("+")) result = a + b;
+                else if (token.equals("-")) result = a - b;
+                else if (token.equals("*")) result = a * b;
+                else if (token.equals("/")) result = a / b;
 
-    } else {
-        // Operand
-        int num = Integer.parseInt(token);
-        stack[++top] = num;
-    }
-}
+                stack[++top] = result;
 
-// Final result
-System.out.println(stack[top]);
+            } else {
+                int num = Integer.parseInt(token);
+                stack[++top] = num;
+            }
+        }
+
+        System.out.println(stack[top]);
+
         // TODO: Evaluate a postfix (Reverse Polish Notation) expression
         //       Operands are non-negative integers, operators are: + - * /
         //       Use a stack to evaluate
@@ -41,8 +41,6 @@ System.out.println(stack[top]);
         // Output: 14
         //
         // Explanation: 5 + ((1+2)*4) - 3 = 5 + 12 - 3 = 14
-
-        String line = sc.nextLine();
 
     }
 }
