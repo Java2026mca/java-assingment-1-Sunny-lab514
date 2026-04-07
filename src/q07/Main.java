c737fd817a899ad7a32dbf251831acba1a2b75e5
Sunny-lab514 <vermasunny502@gmail.com>
2026-04-07 17:23:17 +0530

    Q 07 Solved ( Final_All_Solved_with_All_pass_Test_Cases)

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -6,57 +6,45 @@ public class Main {
         int n = sc.nextInt();
         int[] arr = new int[n];
 
-// Input
-for (int i = 0; i < n; i++) {
-    arr[i] = sc.nextInt();
-}
-
-//  Copy array for sorting
-int[] tempArr = Arrays.copyOf(arr, n);
-
-//  Count swaps (inversion count using simple method)
-int swaps = 0;
-for (int i = 0; i < n; i++) {
-    for (int j = i + 1; j < n; j++) {
-        if (arr[i] > arr[j]) {
-            swaps++;
+        // Input
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-    }
-}
 
-//  Bubble Sort (only for sorting, not counting)
-for (int i = 0; i < n - 1; i++) {
-    for (int j = 0; j < n - i - 1; j++) {
-        if (tempArr[j] > tempArr[j + 1]) {
-            int t = tempArr[j];
-            tempArr[j] = tempArr[j + 1];
-            tempArr[j + 1] = t;
+        int swaps = 0;
+
+        // Optimized Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swaps++;
+                    swapped = true;
+                }
+            }
+
+            // IMPORTANT: early break
+            if (!swapped) break;
         }
-    }
-}
 
-// Print sorted array
-for (int i = 0; i < n; i++) {
-    System.out.print(tempArr[i]);
-    if (i < n - 1) System.out.print(" ");
-}
-System.out.println();
+        // Print sorted array
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+            if (i < n - 1) System.out.print(" ");
+        }
+        System.out.println();
 
-// Print swap count
-System.out.println("Swaps: " + swaps);
+        // Print swaps
+        System.out.println("Swaps: " + swaps);
 
         // TODO: Read n integers into an array
         //       Implement BUBBLE SORT manually (no Arrays.sort)
         //       Print sorted array ascending, space-separated
         //       Then print: "Swaps: X" where X = total number of swaps performed
-        //
-        // Input:
-        // 5
-        // 64 34 25 12 22
-        //
-        // Output:
-        // 12 22 25 34 64
-        // Swaps: 7
-
     }
 }
