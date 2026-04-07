1a6e35220a9202437248f5c60bd98b7efcbf27dc
Sunny-lab514 <vermasunny502@gmail.com>
2026-04-07 16:06:41 +0530

    Solved _Q 07

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -4,49 +4,43 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-          int[] arr = new int[n];
 
-// Input
-for (int i = 0; i < n; i++) {
-    arr[i] = sc.nextInt();
-}
+        int[] arr = new int[n];
 
-int swaps = 0;
-
-// Bubble Sort
-for (int i = 0; i < n - 1; i++) {
-    for (int j = 0; j < n - i - 1; j++) {
-        if (arr[j] > arr[j + 1]) {
-            // swap
-            int temp = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = temp;
-            swaps++;
+        // Read array
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-    }
-}
 
-// Print sorted array
-for (int i = 0; i < n; i++) {
-    System.out.print(arr[i]);
-    if (i < n - 1) System.out.print(" ");
-}
-System.out.println();
+        int swaps = 0;
 
-// Print swap count
-System.out.println("Swaps: " + swaps);
-        // TODO: Read n integers into an array
-        //       Implement BUBBLE SORT manually (no Arrays.sort)
-        //       Print sorted array ascending, space-separated
-        //       Then print: "Swaps: X" where X = total number of swaps performed
-        //
-        // Input:
-        // 5
-        // 64 34 25 12 22
-        //
-        // Output:
-        // 12 22 25 34 64
-        // Swaps: 7
+        // Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
 
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swaps++;
+                    swapped = true;
+                }
+            }
+
+            // Optimization: stop if already sorted
+            if (!swapped) break;
+        }
+
+        // Print sorted array
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+            if (i < n - 1) System.out.print(" ");
+        }
+
+        System.out.println();
+        System.out.println("Swaps: " + swaps);
     }
 }
